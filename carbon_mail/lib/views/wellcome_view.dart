daaebf72855664772d633125ea87a778147dacb7
Ruchan <jceadsa@gmail.com>
2022-02-21 15:59:12 +0300

    Made skeleton
    TODO: Will add server and giving better looking

---
+++ carbon_mail/lib/views/wellcome_view.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import 'mail_list_view.dart';
+
+class WellComeView extends StatelessWidget {
+  const WellComeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: [
+        Image.network(
+          "https://picsum.photos/1080/1920", //For now, TODO place upplab image here
+          fit: BoxFit.cover,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Text(
+                  "Cleaning your inbox has never been easier.",
+                  style: Theme.of(context).textTheme.headline3,
+                ),
+                Text(
+                    "Clean up old subscription and say no to email carbon!") //TODO fix that area
+              ],
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => MailListView(),
+                      ));
+                },
+                child: Text("Get Started  >>")) //TODO fix that area
+          ],
+        )
+      ]),
+    );
+  }
+}
